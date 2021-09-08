e971ba12874ef5c506ca171a095d399f51e0317f
Christopher Rosset <chris@rosset.tech>
2021-09-08 11:54:53 -0400

    Update nix deps

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs; [
+      firefox
       python39Packages.paho-mqtt
       python39Packages.selenium
     ];
